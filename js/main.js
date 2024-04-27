d3ec76bf269ea33456554b2b8ea5802863c30507
thhhrobledo <hantranrobledo@gmail.com>
2024-04-27 15:03:24 +0800

    trial

--- js/main.js
+++ js/main.js
@@ -14,6 +14,32 @@
  limitations under the License.
  */
 
+ // Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      // Capture the registration for later use, if needed
+      let reg;
+
+      // Use ES Module version of our Service Worker in development
+      if (import.meta.env?.DEV) {
+        reg = await navigator.serviceWorker.register('/service-worker.js', {
+          type: 'module',
+        });
+      } else {
+        // In production, use the normal service worker registration
+        reg = await navigator.serviceWorker.register('/service-worker.js');
+      }
+
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
   // Set up the editor
   const { Editor } = await import('./app/editor.js');
--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,43 @@
-/*
-Copyright 2021 Google LLC
+import { offlineFallback, warmStrategyCache } from 'workbox-recipes';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+// Set up page cache
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
 
-      http://www.apache.org/licenses/LICENSE-2.0
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// Set up asset cache
+registerRoute(
+     ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+     new StaleWhileRevalidate({
+       cacheName: 'asset-cache',
+       plugins: [
+         new CacheableResponsePlugin({
+           statuses: [0, 200],
+         }),
+       ],
+     }),
+   );
+
+   // Set up offline fallback
+offlineFallback({
+  pageFallback: '/offline.html',
+});
